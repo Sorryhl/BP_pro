8f1c88c7e8a41eb586903831540e482ea8d0f02e
Sorryhl <1787277322@qq.com>
2021-01-07 19:27:05 +0800

    建立模型，新数据正确率82左右

--- BP_pre/BP_setup.py
+++ BP_pre/BP_setup.py
@@ -11,16 +11,12 @@ if __name__ == '__main__':
     # 获取训练数据
     # trainCases, trainResults = read_traindata()
     trainCases, trainResults = DataReader.read_traindata()
-    # 建立一个归一化器
-    m = MinMaxScaler()
-    # 利用m对data进行归一化，并储存data的归一化参数
-    train_result_m = m.fit_transform(trainResults)
 
     # 建立初始模型, 13个输入层，8个隐含层，1个输出层
     nn.setup(len(trainCases[0]), 8, 1)
 
     # 参数依次为训练数据、训练期望结果、训练次数、学习率、正确率
-    nn.train(trainCases, train_result_m, 10000, 0.05, 0.1)
+    nn.train(trainCases, trainResults, 10000, 0.05, 0.1)
 
     # 保存该新建模型
     joblib.dump(nn, "./BPnn.pkl")
